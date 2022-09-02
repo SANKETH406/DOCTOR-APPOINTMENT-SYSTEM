0f3b021531cfacb7abfeae8348fae2c3ea241a51
GAJIBUNDA SANKETH <SANKETH406>
2022-09-03 01:47:09 +0530

    adding initial files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { AdmincancelComponent } from './pages/admincancel/admincancel.component';
+import { AdminfeedbackComponent } from './pages/adminfeedback/adminfeedback.component';
+import { AdminhomeComponent } from './pages/adminhome/adminhome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegComponent } from './pages/reg/reg.component';
@@ -15,8 +18,11 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'reg',component:RegComponent},
   {path:'userdata',component:UserdataComponent},
-  {path:'userbookings',component:UserbookingsComponent}
-
+  {path:'userbookings',component:UserbookingsComponent},
+  {path:'admincancel',component:AdmincancelComponent},
+  {path:'adminfeedback',component:AdminfeedbackComponent},
+  {path:'adminhome',component:AdminhomeComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
